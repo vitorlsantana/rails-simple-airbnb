9e1b715ee4115eaa8d77a287348ff04b57bf4887
Vitor Santana <santana.vitor@gmail.com>
2020-09-17 18:27:29 -0300

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guestes: 3
+)
+
+Flat.create!(
+  name: 'Apartamento confortável e espaçoso',
+  address: 'SQN 303, Bloco H, Brasília',
+  description: 'Excelente localização, próximo de muitos pontos turísticos da capital brasileira',
+  price_per_night: 150,
+  number_of_guestes: 5
+)
+
+Flat.create!(
+  name: 'Casa com 5 quartos',
+  address: 'Praça da Matriz, Corumbá de Goiás',
+  description: 'Casa espaçosa, com área de lazer completa, com piscina e churrasqueira',
+  price_per_night: 250,
+  number_of_guestes: 10
+)
+
+Flat.create!(
+  name: 'Flat iluminado',
+  address: 'Av. das Araucárias, n.245',
+  description: 'Pequeno apartamento, próximo ao metrô',
+  price_per_night: 50,
+  number_of_guestes: 2
+)
+
+Flat.create!(
+  name: 'Cabana na natureza',
+  address: 'Chapada dos Veadeiros',
+  description: 'Lugar ideal para suas férias próximo à natureza e animais selvagens',
+  price_per_night: 100,
+  number_of_guestes: 3
+)
